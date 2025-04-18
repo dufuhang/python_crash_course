6e0c424043d2790cf09ca009493b4ce5cf180258
dfh <dufuhangse@gmail.com>
2025-04-18 17:15:51 +0800

    update

---
+++ chapter/solutions/chapter_03/3.8.py
@@ -0,0 +1,25 @@
+city = ['shanghai', 'beijing', 'hongkong', 'zhengzhou', 'new york']
+
+print("******************original******************")
+print(city)
+
+print("******************sorted()******************")
+print(sorted(city))
+
+print("******************sorted(reverse=true)******************")
+print(sorted(city, reverse=True))
+print("******************original******************")
+print(city)
+
+print("******************reverse()******************")
+print(city.reverse())
+print("******************original******************")
+print(city)
+
+print("******************sort()******************")
+print(city.sort())
+print("******************original******************")
+print(city)
+
+print(len(city))
+
