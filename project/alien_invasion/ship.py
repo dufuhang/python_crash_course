78087700225c0d072bcab895e47e73bc064346ae
dfh <dufuhangse@gmail.com>
2025-06-13 16:30:15 +0800

    调整飞船速度

--- project/alien_invasion/ship.py
+++ project/alien_invasion/ship.py
@@ -9,6 +9,7 @@ class Ship:
         self.screen = ai_game.screen
         # 获取屏幕的rect属性
         self.screen_rect = ai_game.screen.get_rect()
+        self.settings = ai_game.settings
 
         # 加载飞船图像并获取其外接矩形
         # 这里使用os库中的获取路径的方法，按照书中的代码会提示FileNotFound错误
@@ -21,6 +22,9 @@ class Ship:
         # 每艘飞船最初都在屏幕底部中央
         self.rect.midbottom = self.screen_rect.midbottom
 
+        # 在飞船的属性x中存储一个浮点数
+        self.x = float(self.rect.x)
+
         # 移动标志
         self.moving_right = False
         self.moving_left = False
@@ -28,9 +32,12 @@ class Ship:
     def update(self):
         """根据移动标志调整飞船的位置"""
         if self.moving_right:
-            self.rect.x += 1
+            self.x += self.settings.ship_speed
         if self.moving_left:
-            self.rect.x -= 1
+            self.x -= self.settings.ship_speed
+
+        # 根据self.x更新rect对象
+        self.rect.x = self.x
 
     def blitme(self):
         """在指定位置绘制飞船"""
